ead2f2a64d899ffba55b27fc4551e29038b77288
Dominic Langenhorst <flipi13@web.de>
2019-02-26 08:09:30 +0100

    V0.8.5.1

--- src/blockstone/B1GSt4R/BankCredit/Commands/creditTransferCMD.java
+++ src/blockstone/B1GSt4R/BankCredit/Commands/creditTransferCMD.java
@@ -116,7 +116,78 @@ public class creditTransferCMD implements CommandExecutor {
 					p.sendMessage(plugin.prefix+"§cNo Perms!");
 				}
 			}else {
-				
+				if(!args[0].equals(args[1])) {
+					OfflinePlayer oldPlayer = Bukkit.getOfflinePlayer(args[0]);
+					OfflinePlayer newPlayer = Bukkit.getOfflinePlayer(args[1]);
+					String CreditID = args[2].toString();
+					boolean addieren = args[3].toString().equalsIgnoreCase("addieren");
+					boolean override = args[3].toString().equalsIgnoreCase("überschreiben");
+					
+					if(plugin.api.isCreditExists(CreditID)) {
+						if(plugin.api.isExistsPlayerCredit(oldPlayer, CreditID)) {
+							if(!plugin.api.isExistsPlayerCredit(newPlayer, CreditID)) {
+								String oldPID = plugin.api.builderPlayerUUID_CreditID(oldPlayer, CreditID);
+								plugin.api.createPlayerCredit(newPlayer, CreditID, plugin.api.getNextPayDate(oldPID), plugin.api.getNextPayTime(oldPID), plugin.api.getDaysLeft(oldPID), plugin.api.getRemainingCreditValue(oldPID), plugin.api.getDeferralCounter(oldPID), plugin.api.getNotPayedDays(oldPID), plugin.api.getExtraPays(oldPID), plugin.api.getPunishPays(oldPID));
+								plugin.api.removePlayerCredit(oldPlayer, CreditID);
+								plugin.CONSOLE.sendMessage(plugin.prefix+"Der Spieler §6"+newPlayer.getName()+" §7hat nun den Kredit §6"+CreditID+" §7von §6"+oldPlayer.getName()+" §7bekommen!");
+							}else {
+								if(addieren && !override) {
+									String newPID = plugin.api.builderPlayerUUID_CreditID(newPlayer, CreditID);
+									String oldPID = plugin.api.builderPlayerUUID_CreditID(oldPlayer, CreditID);
+									
+//									String payDate = plugin.api.getNextPayDate(oldPID);
+//									String payTime = plugin.api.getNextPayTime(oldPID);
+									int daysLeft = plugin.api.getDaysLeft(oldPID);
+									double remaining = plugin.api.getRemainingCreditValue(oldPID);
+									int deferrel = plugin.api.getDeferralCounter(oldPID);
+									int notPayedDays = plugin.api.getNotPayedDays(oldPID);
+									double extraPays = plugin.api.getExtraPays(oldPID);
+									double Punishpays = plugin.api.getPunishPays(oldPID);
+									
+									plugin.api.addDeferralCounter(newPID, deferrel);
+									plugin.api.addDaysLeft(newPID, daysLeft);
+									plugin.api.addExtraPays(newPID, extraPays);
+									plugin.api.addNotPayedDays(newPID, notPayedDays);
+									plugin.api.addPunishPay(newPID, Punishpays);
+									plugin.api.addRemainingCreditValue(newPID, remaining);
+									
+									plugin.api.removePlayerCredit(oldPlayer, CreditID);
+									plugin.CONSOLE.sendMessage(plugin.prefix+"Der Spieler §6"+newPlayer.getName()+" §7hat nun den Kredit §6"+CreditID+" §7von §6"+oldPlayer.getName()+" §7addiert bekommen!");
+								}else if(!addieren && override){
+									String newPID = plugin.api.builderPlayerUUID_CreditID(newPlayer, CreditID);
+									String oldPID = plugin.api.builderPlayerUUID_CreditID(oldPlayer, CreditID);
+									
+//									String payDate = plugin.api.getNextPayDate(oldPID);
+//									String payTime = plugin.api.getNextPayTime(oldPID);
+									int daysLeft = plugin.api.getDaysLeft(oldPID);
+									double remaining = plugin.api.getRemainingCreditValue(oldPID);
+									int deferrel = plugin.api.getDeferralCounter(oldPID);
+									int notPayedDays = plugin.api.getNotPayedDays(oldPID);
+									double extraPays = plugin.api.getExtraPays(oldPID);
+									double Punishpays = plugin.api.getPunishPays(oldPID);
+									
+									plugin.api.setDeferralCounter(newPID, deferrel);
+									plugin.api.setDaysLeft(newPID, daysLeft);
+									plugin.api.setExtraPays(newPID, extraPays);
+									plugin.api.setNotPayedDays(newPID, notPayedDays);
+									plugin.api.setPunishPay(newPID, Punishpays);
+									plugin.api.setRemainingCreditValue(newPID, remaining);
+									
+									plugin.api.removePlayerCredit(oldPlayer, CreditID);
+									plugin.CONSOLE.sendMessage(plugin.prefix+"Der Spieler §6"+newPlayer.getName()+" §7hat nun den Kredit §6"+CreditID+" §7von §6"+oldPlayer.getName()+" §7überschrieben bekommen!");
+								}else {
+									plugin.CONSOLE.sendMessage(plugin.prefix+"§c/transfer <oldPlayer> <newPlayer> <CreditID> <addieren|überschreiben>");
+								}
+							}
+						}else {
+							plugin.CONSOLE.sendMessage(plugin.prefix+"Der Spieler §6"+oldPlayer.getName()+" §7hat diesen Kredit nicht! §8[§6"+CreditID+"§8]");
+						}
+					}else {
+						plugin.CONSOLE.sendMessage(plugin.prefix+"Dieser Kredit existiert nicht! §8[§6"+CreditID+"§8]");
+					}
+				}else {
+					plugin.CONSOLE.sendMessage(plugin.prefix+"Bitte wähle nicht zweimal den selben Spieler aus!");
+				}
 			}
 		}
 		return true;
